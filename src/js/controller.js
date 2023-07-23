761c245ae0d127818be4fcad8804aec0a4bc23f1
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-07-23 16:31:11 +0300

    Create search functionality for the app

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,16 @@
+import { async } from "regenerator-runtime";
 import * as model from "./model";
 import recipeView from "./views/recipeView";
+import searchView from "./views/searchView";
+import resultsView from "./views/resultsView";
 
 import "core-js/stable"; // polyfill evrything
 import "regenerator-runtime/runtime"; // polyfill async await
 
 ///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipe = async function () {
   try {
@@ -23,7 +29,26 @@ const controlRecipe = async function () {
   }
 };
 
+const controlSearchResults = async () => {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 const init = () => {
   recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
